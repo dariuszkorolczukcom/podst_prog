ffa0e01c3817f3f9f889247aa2a7f27bde23d738
Dariusz Korolczuk <dariuszkorolczuk@MacBook-Pro-Dariusz.local>
2023-12-03 12:39:48 +0100

    6-2

--- zajęcia6/6-2.c
+++ zajęcia6/6-2.c
@@ -3,33 +3,31 @@
 #include <ctype.h>
 
 void show(int * t, int N);
-int* wypelnij_tablice_losowo(int n);
-int* wypelnij_tablice_od_uzytkownika(int n);
-int* wypelnij_tablice_wartoscia(int n, int w);
+int* iloczyn(int n, int *wektor, int a);
 
 int main() {
-   int N, w;
+   int N, *wektor, i, a;
    // pobiera wielkosc tablicy
-   printf("Podaj wielkość tablicy: ");
+   printf("Podaj wielkość wektora: ");
    scanf("%d",&N);
-   printf("Podaj liczbe w: ");
-   scanf("%d",&w);
    if(N<=0) 
    {
       printf("\ntablica musi być większa niz 0\n");
       return 0;
    }
 
+   printf("Podaj %d liczb wektora\n", N);
+   for (i=0;i<N;++i)
+   {
+      scanf("%d", &wektor[i]);
+   }
+   show(wektor, N);
 
-   int * t =  wypelnij_tablice_losowo(N);
+   printf("Podaj %d liczbe a\n", N);
+      scanf("%d", &a);
+
+   int * t = iloczyn(N, wektor, a);
    show(t, N);
-   free(t);
-   t = wypelnij_tablice_od_uzytkownika(N);
-   show(t, N);
-   free(t);
-   t = wypelnij_tablice_wartoscia(N, w);
-   show(t, N);
-   free(t);
 
    return 0;
 }
@@ -37,59 +35,22 @@ int main() {
 void show(int * t, int N) {
    int i;
    //wyswietla tablice t
-   printf("[%d,",t[0]);
+   printf("{%d,",t[0]);
    for (i=1;i<N-1;++i)
    {
       printf("%d,", t[i]);
    }
-   printf("%d]\n",t[N-1]);
+   printf("%d}\n",t[N-1]);
 }
 
-int* wypelnij_tablice_wartoscia(int N, int w) {
-   //deklaracja zmiennych
-   int *t = (int*)malloc(N * sizeof(int));
-
+int* iloczyn(int n, int *wektor, int a) {
    int i;
-   //wsad danych do tablicy
-   printf("Podaj %d liczb\n", N);
-   for (i=0;i<N;++i)
-   {
-      t[i] = w;
+   for (i=0;i<n;++i) {
+      wektor[i] *= a;
    }
-
-   return t;
+   return wektor;
 }
-
-int* wypelnij_tablice_od_uzytkownika(int N) {
-   //deklaracja zmiennych
-   int *t = (int*)malloc(N * sizeof(int));
-
-   int i;
-   //wsad danych do tablicy
-   printf("Podaj %d liczb\n", N);
-   for (i=0;i<N;++i)
-   {
-      scanf("%d", &t[i]);
-   }
-
-   return t;
-}
-
-int* wypelnij_tablice_losowo(int N) {
-   //deklaracja zmiennych
-   int *t = (int*)malloc(N * sizeof(int));
-
-   int i;
-
-   for(i=0;i<N;i++)
-   t[i]=rand()% 100 + 1;
-
-   return t;
-}
-
-// Zadanie 6.1
-// Napisz trzy funkcje wypełniające i zwracające n-elementową tablicę liczb całkowitych:
-
-// int* wypelnij_tablice_losowo(int n) - wartościami losowymi,
-// int* wypelnij_tablice_od_uzytkownika(int n)- wartościami pobranymi od użytkownika,
-// int* wypelnij_tablice_wartoscia(int n, int w) - wartością w.
+// Zadanie 6.2
+// Napisz funkcję int* iloczyn(int n, int *wektor, int a), która liczy iloczyn liczby a przez wektor o długości n (wynikiem jest wektor o długości n).
+// przykład: wektor = {2, 4, 3, 1}, a = 2
+// wynik: {4, 8, 6, 2}
